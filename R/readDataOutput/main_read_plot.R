4bcf4201f0bb81b59fc0bee609812f8e3b8a9f3f
Jorge Flores Valiente <jorgefloresvaliente@gmail.com>
2020-01-24 09:04:49 +0000

    xXx

--- R/readDataOutput/main_read_plot.R
+++ R/readDataOutput/main_read_plot.R
@@ -6,10 +6,10 @@
 # Aim    : 
 # URL    : 
 #=============================================================================#
-dirpath <- 'E:/kinesis_escenarios_outputs/escenario_t-4c2m2/outM8/'
+dirpath <- 'E:/kinesis_escenarios_outputs/escenario_t4c0.5m0.5/outM2/'
 nfiles  <- length(list.files(path = dirpath, pattern = paste0('output','.*\\.txt'), full.names = T, recursive = T))
 max_paticles <- 9540
-stepToPlot   <- c(21,90,360)
+stepToPlot   <- c(360)
 stepFinal    <- 360
 ylimknob     <- c(0,16)
 ylimweight   <- c(0,16)
@@ -56,24 +56,24 @@ if(file.exists(x = paste0(dirpath, 'df.RData'))){
 #=============================================================================#
 #--- Plot Density Map ---#
 dens2D_map_byage(df = df, outpath = outpathFigures, ages = stepToPlot)
-dens2D_map_byage(df = df, outpath = outpathFigures, ages = stepToPlot, xlimmap = c(-85,-70), ylimmap = c(-20,0), alive = T)
+dens2D_map_byage(df = df, outpath = outpathFigures, ages = stepToPlot, alive = T)
 
-dens2D_map_byday(df = df, outpath = outpathFigures, days = stepToPlot)
-dens2D_map_byday(df = df, outpath = outpathFigures, days = stepToPlot, xlimmap = c(-85,-70), ylimmap = c(-20,0), alive = T)
+# dens2D_map_byday(df = df, outpath = outpathFigures, days = stepToPlot)
+# dens2D_map_byday(df = df, outpath = outpathFigures, days = stepToPlot, alive = T)
 
 #--- Plot Knob Map ---#
 knob_map_byage(df = df, outpath = outpathFigures, ages = c(stepToPlot))
-knob_map_byage(df = df, outpath = outpathFigures, ages = c(stepToPlot), xlimmap = c(-85,-70), ylimmap = c(-20,0), alive = T)
+knob_map_byage(df = df, outpath = outpathFigures, ages = c(stepToPlot), alive = T)
 
-knob_map_byday(df = df, outpath = outpathFigures, days = c(stepToPlot))
-knob_map_byday(df = df, outpath = outpathFigures, days = c(stepToPlot), xlimmap = c(-85,-70), ylimmap = c(-20,0), alive = T)
+# knob_map_byday(df = df, outpath = outpathFigures, days = c(stepToPlot))
+# knob_map_byday(df = df, outpath = outpathFigures, days = c(stepToPlot), alive = T)
 
 # Plot Weight Map ---#
 weight_map_byage(df = df, outpath = outpathFigures, ages = c(stepToPlot), zlimmap = c(0,23))
-weight_map_byage(df = df, outpath = outpathFigures, ages = c(stepToPlot), xlimmap = c(-85,-70), ylimmap = c(-20,0), alive = T, zlimmap = c(0,23))
+weight_map_byage(df = df, outpath = outpathFigures, ages = c(stepToPlot), zlimmap = c(0,23), alive = T)
 
-weight_map_byday(df = df, outpath = outpathFigures, days = c(stepToPlot), zlimmap = c(0,23))
-weight_map_byday(df = df, outpath = outpathFigures, days = c(stepToPlot), xlimmap = c(-85,-70), ylimmap = c(-20,0), alive = T, zlimmap = c(0,23))
+# weight_map_byday(df = df, outpath = outpathFigures, days = c(stepToPlot), zlimmap = c(0,23))
+# weight_map_byday(df = df, outpath = outpathFigures, days = c(stepToPlot), zlimmap = c(0,23), alive = T)
 
 #--- Plot Particle Maps ---#
 if(length(list.files(outpathByDayAll)) != nfiles)   MapParticlesByDay(df = df, outpath = outpathByDayAll)
@@ -82,39 +82,39 @@ if(length(list.files(outpathByDayAlive)) != nfiles) MapParticlesByDay(df = df, o
 if(length(list.files(outpathByAgeAll)) != nfiles)   MapParticlesByAge(df = df, outpath = outpathByAgeAll)
 if(length(list.files(outpathByAgeAlive)) != nfiles) MapParticlesByAge(df = df, outpath = outpathByAgeAlive, alive = T)
 
-#=============================================================================#
-# Plot Mean Growth (byday) with Wweight
-#=============================================================================#
-knob_weight_byday(df = df, day = stepFinal, VB = VB40, alive = F)
-knob_weight_byday(df = df, day = stepFinal, VB = VB40, alive = T)
-
-png(file = paste0(dirpath, 'figures/meanKnobWeightByday.png'), width = 650, height = 650)
-par(lwd = 2, mar = c(4.5,4.5,1,4.5))
-plot(1:stepFinal, mean_knob_byday[1:stepFinal],type = 'l', xlab = '', ylab = '', lwd = 2, axes = F, ylim = ylimknob)
-par(new = T, lwd = 2)
-plot(1:stepFinal, mean_weight_byday[1:stepFinal],type = 'l', col = 'red', axes = F, xlab = '', ylab = '', ylim = ylimweight)
-box(lwd = 2)
-axis(side = 1, lwd = 2, lwd.ticks = 2, font.axis = 4, cex.axis = 1.5)
-axis(side = 2, lwd = 2, lwd.ticks = 2, font.axis = 4, cex.axis = 1.5, las = 2)
-axis(side = 4, lwd = 2, lwd.ticks = 2, font.axis = 4, cex.axis = 1.5, las = 2, col='red',  col.axis = 'red')
-mtext(side = 1, line = 3, font = 2, cex = 1.5, text = 'Days after spawning')
-mtext(side = 2, line = 3, font = 2, cex = 1.5, text = 'knob (cm)')
-mtext(side = 4, line = 3, font = 2, cex = 1.5, text = 'Wet Weight (g)', col = 'red')
-dev.off()
-
-png(file = paste0(dirpath, 'figures/meanKnobWeightBydayAlive.png'), width = 650, height = 650)
-par(lwd = 2, mar = c(4.5,4.5,1,4.5))
-plot(1:stepFinal, mean_knob_byday_alive[1:stepFinal],type = 'l', xlab = '', ylab = '', lwd = 2, axes = F, ylim = ylimknob)
-par(new = T, lwd = 2)
-plot(1:stepFinal, mean_weight_byday_alive[1:stepFinal],type = 'l', col = 'red', axes = F, xlab = '', ylab = '', ylim = ylimweight)
-box(lwd = 2)
-axis(side = 1, lwd = 2, lwd.ticks = 2, font.axis = 4, cex.axis = 1.5)
-axis(side = 2, lwd = 2, lwd.ticks = 2, font.axis = 4, cex.axis = 1.5, las = 2)
-axis(side = 4, lwd = 2, lwd.ticks = 2, font.axis = 4, cex.axis = 1.5, las = 2, col='red',  col.axis = 'red')
-mtext(side = 1, line = 3, font = 2, cex = 1.5, text = 'Days after spawning')
-mtext(side = 2, line = 3, font = 2, cex = 1.5, text = 'knob (cm)')
-mtext(side = 4, line = 3, font = 2, cex = 1.5, text = 'Wet Weight (g)', col = 'red')
-dev.off()
+# #=============================================================================#
+# # Plot Mean Growth (byday) with Wweight
+# #=============================================================================#
+# knob_weight_byday(df = df, day = stepFinal, VB = VB40, alive = F)
+# knob_weight_byday(df = df, day = stepFinal, VB = VB40, alive = T)
+# 
+# png(file = paste0(dirpath, 'figures/meanKnobWeightByday.png'), width = 650, height = 650)
+# par(lwd = 2, mar = c(4.5,4.5,1,4.5))
+# plot(1:stepFinal, mean_knob_byday[1:stepFinal],type = 'l', xlab = '', ylab = '', lwd = 2, axes = F, ylim = ylimknob)
+# par(new = T, lwd = 2)
+# plot(1:stepFinal, mean_weight_byday[1:stepFinal],type = 'l', col = 'red', axes = F, xlab = '', ylab = '', ylim = ylimweight)
+# box(lwd = 2)
+# axis(side = 1, lwd = 2, lwd.ticks = 2, font.axis = 4, cex.axis = 1.5)
+# axis(side = 2, lwd = 2, lwd.ticks = 2, font.axis = 4, cex.axis = 1.5, las = 2)
+# axis(side = 4, lwd = 2, lwd.ticks = 2, font.axis = 4, cex.axis = 1.5, las = 2, col='red',  col.axis = 'red')
+# mtext(side = 1, line = 3, font = 2, cex = 1.5, text = 'Days after spawning')
+# mtext(side = 2, line = 3, font = 2, cex = 1.5, text = 'knob (cm)')
+# mtext(side = 4, line = 3, font = 2, cex = 1.5, text = 'Wet Weight (g)', col = 'red')
+# dev.off()
+# 
+# png(file = paste0(dirpath, 'figures/meanKnobWeightBydayAlive.png'), width = 650, height = 650)
+# par(lwd = 2, mar = c(4.5,4.5,1,4.5))
+# plot(1:stepFinal, mean_knob_byday_alive[1:stepFinal],type = 'l', xlab = '', ylab = '', lwd = 2, axes = F, ylim = ylimknob)
+# par(new = T, lwd = 2)
+# plot(1:stepFinal, mean_weight_byday_alive[1:stepFinal],type = 'l', col = 'red', axes = F, xlab = '', ylab = '', ylim = ylimweight)
+# box(lwd = 2)
+# axis(side = 1, lwd = 2, lwd.ticks = 2, font.axis = 4, cex.axis = 1.5)
+# axis(side = 2, lwd = 2, lwd.ticks = 2, font.axis = 4, cex.axis = 1.5, las = 2)
+# axis(side = 4, lwd = 2, lwd.ticks = 2, font.axis = 4, cex.axis = 1.5, las = 2, col='red',  col.axis = 'red')
+# mtext(side = 1, line = 3, font = 2, cex = 1.5, text = 'Days after spawning')
+# mtext(side = 2, line = 3, font = 2, cex = 1.5, text = 'knob (cm)')
+# mtext(side = 4, line = 3, font = 2, cex = 1.5, text = 'Wet Weight (g)', col = 'red')
+# dev.off()
 
 #=============================================================================#
 # Plot Mean Growth (byage) with Wweight
@@ -150,32 +150,32 @@ mtext(side = 2, line = 3, font = 2, cex = 1.5, text = 'knob (cm)')
 mtext(side = 4, line = 3, font = 2, cex = 1.5, text = 'Wet Weight (g)', col = 'red')
 dev.off()
 
-#=============================================================================#
-# Hist Mean Growth (alive_byday) with Wweight
-#=============================================================================#
-hist_knob_weight_byday(df = df, Day = stepFinal, VB = VB40)
-
-if(length(hist_knob_byday) != 0 | length(hist_weight_byday) != 0){
-  png(file = paste0(dirpath, 'figures/histKnobWeightByday.png'), width = 950, height = 550)
-  par(lwd = 2, mfrow = c(1,2), mar = c(4.5,4.5,1,4.5))
-  
-  hist(hist_knob_byday  , freq = F, ylim = c(0,.6), axes = F, xlab = '', ylab = '', main = '')
-  axis(side = 1, lwd = 2, lwd.ticks = 2, font.axis = 4, cex.axis = 1.5)
-  axis(side = 2, lwd = 2, lwd.ticks = 2, font.axis = 4, cex.axis = 1.5, las = 2)
-  mtext(side = 1, line = 3, font = 2, cex = 1.5, text = 'knob (cm)')
-  mtext(side = 2, line = 3, font = 2, cex = 1.5, text = 'Relative Frequecy (%)')
-  mtext(side = 3, line =-2, font = 2, cex = 1.5, text = paste('Day:', stepFinal), adj = 0.1)
-  box(lwd = 2)
-  
-  hist(hist_weight_byday, freq = F, ylim = c(0,.6), axes = F, xlab = '', ylab = '', main = '')
-  axis(side = 1, lwd = 2, lwd.ticks = 2, font.axis = 4, cex.axis = 1.5)
-  axis(side = 2, lwd = 2, lwd.ticks = 2, font.axis = 4, cex.axis = 1.5, las = 2)
-  mtext(side = 1, line = 3, font = 2, cex = 1.5, text = 'Wet Weight (g)')
-  mtext(side = 2, line = 3, font = 2, cex = 1.5, text = 'Relative Frequecy (%)')
-  mtext(side = 3, line =-2, font = 2, cex = 1.5, text = paste('Day:', stepFinal), adj = 0.1)
-  box(lwd = 2)
-  dev.off()
-}
+# #=============================================================================#
+# # Hist Mean Growth (alive_byday) with Wweight
+# #=============================================================================#
+# hist_knob_weight_byday(df = df, Day = stepFinal, VB = VB40)
+# 
+# if(length(hist_knob_byday) != 0 | length(hist_weight_byday) != 0){
+#   png(file = paste0(dirpath, 'figures/histKnobWeightByday.png'), width = 950, height = 550)
+#   par(lwd = 2, mfrow = c(1,2), mar = c(4.5,4.5,1,4.5))
+#   
+#   hist(hist_knob_byday  , freq = F, ylim = c(0,.6), axes = F, xlab = '', ylab = '', main = '')
+#   axis(side = 1, lwd = 2, lwd.ticks = 2, font.axis = 4, cex.axis = 1.5)
+#   axis(side = 2, lwd = 2, lwd.ticks = 2, font.axis = 4, cex.axis = 1.5, las = 2)
+#   mtext(side = 1, line = 3, font = 2, cex = 1.5, text = 'knob (cm)')
+#   mtext(side = 2, line = 3, font = 2, cex = 1.5, text = 'Relative Frequecy (%)')
+#   mtext(side = 3, line =-2, font = 2, cex = 1.5, text = paste('Day:', stepFinal), adj = 0.1)
+#   box(lwd = 2)
+#   
+#   hist(hist_weight_byday, freq = F, ylim = c(0,.6), axes = F, xlab = '', ylab = '', main = '')
+#   axis(side = 1, lwd = 2, lwd.ticks = 2, font.axis = 4, cex.axis = 1.5)
+#   axis(side = 2, lwd = 2, lwd.ticks = 2, font.axis = 4, cex.axis = 1.5, las = 2)
+#   mtext(side = 1, line = 3, font = 2, cex = 1.5, text = 'Wet Weight (g)')
+#   mtext(side = 2, line = 3, font = 2, cex = 1.5, text = 'Relative Frequecy (%)')
+#   mtext(side = 3, line =-2, font = 2, cex = 1.5, text = paste('Day:', stepFinal), adj = 0.1)
+#   box(lwd = 2)
+#   dev.off()
+# }
 
 #=============================================================================#
 # Hist Mean Growth (alive_byage) with Wweight
@@ -227,14 +227,19 @@ for(i in 1:stepFinal){
 }
 
 png(filename = paste0(outpathFigures,'survival.png'), width = 850, height = 850, res = 120)
-plot(PA_sur, type = 'l', lty = 2, ylab = 'Survival', xlab = 'Age', ylim = c(0,100))
+par(lwd = 2, mar = c(4.5,4.5,1,1))
+plot(PA_sur, type = 'l', lty = 2, ylim = c(0,100), axes = F, xlab = '', ylab = '')
 lines(abs(sur_serie - 100), type = 'l', col = 'red')
 legend('topright', legend = c('40%+Z', '40%'), bty = 'n', lty = c(2,1), col = c('black','red'))
+box(lwd = 2)
+axis(side = 1, lwd = 2, lwd.ticks = 2, font.axis = 4, cex.axis = 1.5)
+axis(side = 2, lwd = 2, lwd.ticks = 2, font.axis = 4, cex.axis = 1.5, las = 2)
+mtext(side = 1, line = 3, font = 2, cex = 1.5, text = 'Age in Days')
+mtext(side = 2, line = 3, font = 2, cex = 1.5, text = 'Survival')
 dev.off()
 
-
 # Plotear serie de tiempo por variable
-if(length(hist_knob_byday) != 0 | length(hist_weight_byday) != 0){
+if(length(hist_knob_byage) != 0 | length(hist_weight_byage) != 0){
   varis <- c('exSST', 'exPY', 'exSZ', 'exMZ')
   png(filename = paste0(outpathFigures, 'varitimeserie.png'), width = 1250, height = 950, res = 120)
   par(mfrow = c(2,2), mar = c(3.5,5,1,1))
