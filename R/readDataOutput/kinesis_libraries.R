d25a46236aa2cc3b3e719039d49c1d15a61ff6c5
Jorge Flores Valiente <jorgefloresvaliente@gmail.com>
2020-12-16 04:15:53 +0000

    llamar librerias

---
+++ R/readDataOutput/kinesis_libraries.R
@@ -0,0 +1,30 @@
+#=============================================================================#
+# Name   : kinesis_libraries
+# Author : Jorge Flores
+# Date   : 
+# Version:
+# Aim    : load libraries
+# URL    : 
+#=============================================================================#
+
+library(ncdf4)
+library(R.matlab)
+library(XML)
+library(stringr)
+
+library(ggplot2)
+library(gridExtra)
+library(fields)
+library(maps)
+library(mapdata)
+library(vcd)
+
+library(sp)
+library(raster)
+library(akima)
+library(mgcv)
+library(Hmisc)
+
+#=============================================================================#
+# END OF PROGRAM
+#=============================================================================#
